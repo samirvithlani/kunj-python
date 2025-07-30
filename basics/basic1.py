1c0609d85d010e3e0a6355f504e8c53b0eb1319f
samirvithlani <samir.vithlani83955@gmail.com>
2025-07-30 12:46:33 +0530

    basics

---
+++ basics/basic1.py
@@ -0,0 +1,29 @@
+print("Hello All")
+print("ok",end=",")
+print("Hi")
+
+#indentation
+print("ok")
+
+age =20
+print("age = ",age)
+print(f"age = {age}")
+print(type(age)) #int
+print(type(age).__name__)
+
+pers = 77.89
+print(f"pers = {pers}")
+print(f"type of pers = {type(pers)}")
+
+name = 'royal'
+print(f"name = {name}")
+print(f"type of name = {type(name)}") #str
+
+isActive = True #False
+print("isActive = ",isActive)
+print(f"type of isActive = {type(isActive)}")
+
+#void None
+x=None
+print(x)
+print(f"type od x = {type(x)}")
---
+++ basics/basic2.py
@@ -0,0 +1,22 @@
+#scanf promt scanner, cin,-input
+
+# no = input("enter no :")
+# print(no)
+# print(type(no))
+
+no = int(input("enter no :"))
+print(no)
+print(type(no))
+
+pers = float(input("enter pers : "))
+print(pers)
+
+#int  --> samir
+#string == > "12" "najshgsaosahbas"
+
+
+flag = bool(input("enter boolean value..."))
+print("flag  =",flag)
+
+name = input("enter name :")
+print(name)
