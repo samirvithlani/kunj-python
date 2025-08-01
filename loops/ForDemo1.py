9506bed5012bdb8dad36b2098feacb0b8cd97f98
samirvithlani <samir.vithlani83955@gmail.com>
2025-08-01 12:15:58 +0530

    loop

---
+++ loops/ForDemo1.py
@@ -0,0 +1,20 @@
+'''
+
+range(1,10) --> [1,,2,..10]
+i++
+i=1
+1 in 1 to 10
+
+'''
+# for i in range(1,11):
+#     print(i,end=" ")
+
+# for i in range(11):
+#     print(i,end=" ")
+
+# for i in range(1,21,2):
+#     print(i,end=" ")
+
+
+for i in range(10,0,-1):
+    print(i,end=" ")
