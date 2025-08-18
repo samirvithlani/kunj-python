eed4a65ff129c73366baf58e4375f5d448981776
samirvithlani <samir.vithlani83955@gmail.com>
2025-08-18 12:26:34 +0530

    function

---
+++ string/StringDemo3.py
@@ -0,0 +1,12 @@
+name = "royal"
+
+name = name.upper()
+print(name)
+name = name.lower()
+print(name)
+
+sent = "hI tHis iS iNDia"
+#sent = sent.capitalize()
+sent =sent.title()
+#sent = sent.swapcase()
+print(sent)
