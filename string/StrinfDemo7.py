376556913ce6fd51980f20d35624feffb89dc151
samirvithlani <samir.vithlani83955@gmail.com>
2025-08-20 12:51:04 +0530

    string slicing

---
+++ string/StrinfDemo7.py
@@ -0,0 +1,8 @@
+data = "204 surbhi complex opp municipal market cg road"
+print(data)
+x = data.split(" ")
+print(x)
+
+
+data = data.join("a")
+print(data)
---
+++ string/StrinfDemo8.py
@@ -0,0 +1,18 @@
+#slicing..
+#python supoort neg indexing
+
+data = "hi ok"
+
+print(data[0])
+print(data[-5])
+
+
+city = "AHMEDABAD"
+#[strating:ending:steps]
+print(city[::]) #0 ->end
+print(city[1:3])
+print(city[1:8:2]) 
+print(city[::-1]) #reverse..
+print(city[0:-1])
+print(city[-1:5:-1])
+print(city[-1::1]) #increment
