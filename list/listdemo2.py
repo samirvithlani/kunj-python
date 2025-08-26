c917f140f7ad486445dc2e30720fc2dfcaa6f9ba
samirvithlani <samir.vithlani83955@gmail.com>
2025-08-26 16:19:54 +0530

    list

---
+++ list/listdemo2.py
@@ -0,0 +1,15 @@
+marks = [22,23,45,67,89,90,100,100,23]
+
+cnt = marks.count(1000)
+print("count",cnt)
+
+#marks.reverse()
+#print(marks)
+
+#marks.sort()
+#marks.sort(reverse=True)
+#print(marks)
+
+x = marks.index(899)
+print(x)
+
