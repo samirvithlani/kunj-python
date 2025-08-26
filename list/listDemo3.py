3044c36d04a0dfc7a6a94154660c698d1ad1484d
samirvithlani <samir.vithlani83955@gmail.com>
2025-08-26 16:48:14 +0530

    list

---
+++ list/listDemo3.py
@@ -0,0 +1,30 @@
+data = [[10,20],[100,200],[100,20]]
+
+print(data[0])
+print(data[0][1])
+
+# for i in range(0,len(data)):
+#     #print(data[i])
+#     for j in range(0,len(data[i])):
+#         print(data[i][j],end=" ")
+#     print()    
+    
+# for i in data:
+#     for j in i:
+#         print(j,end=" ")    
+#     print()    
+
+# for i,j in data:
+#     print(i,j,end=" ")
+
+#task:
+#list =[[10,20,30],[100,200,300],[400,500,600]]
+#[60,600,1500]
+
+#--->continue -- max elment - 1500
+#find index of 1500 -- 2 -- >3rd day sales is higher...
+
+#list =[[10,20,30],[100,200,300],[400,500,600]]
+
+#get row and column from user and remove that elemnt apply required validation
+
