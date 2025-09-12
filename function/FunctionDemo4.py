cda493d58684139e7a1f177d7951e221d6a43a1d
samirvithlani <samir.vithlani83955@gmail.com>
2025-09-12 12:40:32 +0530

    data

---
+++ function/FunctionDemo4.py
@@ -0,0 +1,6 @@
+def getData(**kwargs):
+    #dict
+    print(kwargs)
+
+
+getData(name="sam",age=21,city="ahm",x=100)    
---
+++ list/zipDemo1.py
@@ -0,0 +1,6 @@
+list1 = [1,2,3]
+list2 = [11,22,33]
+
+
+for i,j in zip(list1,list2):
+    print(i,j)
